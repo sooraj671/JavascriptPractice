4bd68df02b890755275daacdfda68cb92162f445
sooraj671 <75793312+sooraj671@users.noreply.github.com>
2022-08-21 04:14:42 +0500

    updatedPractice
    Signed-off-by: sooraj671 <75793312+sooraj671@users.noreply.github.com>

---
+++ js/variables.js
@@ -0,0 +1,27 @@
+let num = 10;
+console.log('Before value of num: '+num);
+let name = 'sooraj';
+console.log('Before value of name: '+name);
+let price = 12.5;
+console.log('Before value of price: '+price);
+num = 20;
+console.log('After value of num: '+num);
+name = 'kumar';
+console.log('After value of name: '+name);
+price = 20.5;
+console.log('After value of price: '+price);
+
+let firstname = 'soorajkumar'
+console.log('firstname[0] :'+firstname[0]);
+firstname[0] = 'k';
+console.log('firstname[0] :'+firstname[0]);
+console.log('firstname: '+firstname)
+
+const count = 5;
+console.log(typeof count);
+
+//will cause TypeError because trying to change const value
+count = 10;
+console.log('After value of count: '+count);
+
+
